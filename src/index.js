7c0ae350c385126d5c5d9acab3436cefbbd6c8b3
strbytes <strbytes@gmail.com>
2022-09-10 20:10:06 -0400

    Add nav bar and basic style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 const content = document.querySelector("#content");
 
+const createNavBar = function() {
+  const navBar = document.createElement("div");
+  navBar.id = "navbar";
+  const navList = document.createElement("ul");
+
+  for (menuItem of ["Main", "Menu", "Contact"]) {
+    const listItem = document.createElement("li");
+    listItem.textContent = menuItem;
+    navList.appendChild(listItem);
+
+    listItem.addEventListener("click", _ => {
+      console.log(_.target.textContent);
+    })
+  }
+
+  navBar.appendChild(navList);
+  content.appendChild(navBar);
+}()
+
