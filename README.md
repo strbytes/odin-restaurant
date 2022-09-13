da1ee3eabe31369d39c5d27f9c7cc9258431c656
strbytes <strbytes@gmail.com>
2022-09-12 20:14:06 -0400

    Add home page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin-restaurant
 Restaurant page project for TOP
+
+https://commons.wikimedia.org/wiki/File:Barbieri_-_ViaSophia25668.jpg
+
--- src/home.js
+++ src/home.js
@@ -1,6 +1,23 @@
 const createHomePage = function() {
   const page = document.createElement("div");
-  page.textContent = "Home";
+  page.id = "home";
+
+  const name = document.createElement("h1");
+  name.textContent = "Rrrestaurante!!"
+  page.appendChild(name);
+
+  const address1 = document.createElement("p");
+  address1.textContent = "100 One Hundred Road";
+  page.appendChild(address1);
+
+  const address2 = document.createElement("p");
+  address2.textContent = "East Hairsville, GA 10001";
+  page.appendChild(address2);
+  
+  const image = new Image();
+  image.src = './restaurante.jpg';
+  page.appendChild(image);
+
   return page;
 }
 
